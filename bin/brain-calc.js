669e728fdc6ba5c08a61f5b3bd775762eed5487d
ksuan <cyca005@mail.ru>
2024-01-08 12:49:17 +0300

    stap 6 done

---
+++ bin/brain-calc.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import startGameCalc from '../src/game/game-calc.js';
+
+startGameCalc();
--- src/game/game-calc.js
+++ src/game/game-calc.js
@@ -1,5 +1,5 @@
 import logicGame from '../index.js';
-import { getRandomNumber } from '../src/formula.js';
+import { getRandomNumber } from '../formula.js';
 
 const rule = 'What is the result of the expression?';
 const operations = ['+', '-', '*'];
@@ -21,10 +21,19 @@ let answer;
 return answer;
 };
 
-const getQestionAnswer = () => {
+const getQuestionAnswer = () => {
 const oper1 = getRandomNumber();
 const oper2 = getRandomNumber();
-const operation =  
-}
+const operation = operations[getRandomNumber(0, operations.length)];
+const question = `${oper1} ${operation} ${oper2}`;
+const answer = getQuestion(oper1, oper2, operation);
+return [question, String(answer)];   
+};
+
+const startGameCalc = () => {
+ logicGame(rule, getQuestionAnswer);
+};
+
+export default startGameCalc;
 
 
